0cd4c94e607325fdfdd2220e77ea1255b6a4273e
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2023-01-30 09:41:18 +0100

    form classes

--- controllers/ContactFormController.php
+++ controllers/ContactFormController.php
@@ -1,1 +1,41 @@
 <?php
+
+namespace App\Controllers;
+
+use App\Models\ContactForm;
+use App\Services\ContactFormService;
+
+class ContactFormController
+{
+    protected $contactFormService;
+
+    public function __construct(ContactFormService $contactFormService)
+    {
+        $this->contactFormService = $contactFormService;
+    }
+
+    public function create()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = [
+                'name' => $_POST['name'],
+                'email' => $_POST['email'],
+                'subject' => $_POST['subject'],
+                'message' => $_POST['message']
+            ];
+
+            $contactForm = new ContactForm($data);
+
+            if ($this->contactFormService->validate($contactForm)) {
+                $this->contactFormService->store($contactForm);
+
+                // redirect to a success page
+                header('Location: /success');
+                exit();
+            }
+        }
+
+        // render the contact form view
+        require __DIR__ . '/../views/contactform/create.php';
+    }
+}
--- models/ContactForm.php
+++ models/ContactForm.php
@@ -1,1 +1,51 @@
 <?php
+
+class ContactForm
+{
+    private $name;
+    private $email;
+    private $message;
+
+    public function __construct($name, $email, $message)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->message = $message;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+}
+
